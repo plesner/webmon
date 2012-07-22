9f7b7a24cceb308e99a910eb5689c08af915f509
Christian Plesner Hansen <plesner@ne.utrino.org>
2012-07-22 17:09:35 +1000

    Made the popup do automatic updating and fixed up the ui a tiny bit.

--- chrome/background.js
+++ chrome/background.js
@@ -1,4 +1,36 @@
-var connections = [];
+function PageRegistry() {
+  this.pageMap = {};
+}
+
+PageRegistry.INSTANCE = new PageRegistry();
+
+/**
+ * Returns the singleton page registry instance.
+ */
+PageRegistry.get = function () {
+  return this.INSTANCE;
+};
+
+/**
+ * Records that there is a connection to the tab with the given id.
+ */
+PageRegistry.prototype.register = function (tabId, connection) {
+  this.pageMap[tabId] = connection;
+};
+
+/**
+ * Removes the record of the connection to the tab with the given id.
+ */
+PageRegistry.prototype.unregister = function (tabId) {
+  delete this.pageMap[tabId];
+};
+
+/**
+ * Returns the connection to the given tab.
+ */
+PageRegistry.prototype.getConnection = function (tabId) {
+  return this.pageMap[tabId];
+};
 
 /**
  * A connection between this background page and a page using webmon.
@@ -7,6 +39,15 @@ function PageConnection(port) {
   this.port = port;
   this.callback = null;
   this.port.onMessage.addListener(this._handleResponse.bind(this));
+  PageRegistry.get().register(this.getTabId(), this);
+  this.port.onDisconnect.addListener(this._handleDisconnect.bind(this));
+}
+
+/**
+ * Returns the tab id of the tab this connection connects to.
+ */
+PageConnection.prototype.getTabId = function () {
+  return this.port.sender.tab.id;
 }
 
 /**
@@ -26,9 +67,36 @@ PageConnection.prototype._handleResponse = function (message) {
   }
 };
 
-function onConnected(connection, sender) {
-  connections.push(connection);
-  chrome.pageAction.show(sender.tab.id);
+PageConnection.prototype._handleDisconnect = function () {
+  PageRegistry.get().unregister(this.getTabId());
+};
+
+/**
+ * Called when a new connection is created.
+ */
+function onConnected(connection) {
+  var tabId = connection.getTabId();
+  chrome.pageAction.show(tabId);
+  // Holy smokes batman, that's some cheesy hackery!
+  chrome.pageAction.setPopup({
+    popup: "popup.html?tabId=" + tabId,
+    tabId: tabId
+  });
+}
+
+/**
+ * Called when we display a popup.
+ */
+function onPopup(tabId, sendResponse) {
+  var connection = PageRegistry.get().getConnection(tabId);
+  if (!connection) {
+    sendResponse();
+  } else {
+    connection.send(["getVariables"], function (response) {
+      sendResponse(response);
+    });
+    return true;
+  }
 }
 
 function onLoad() {
@@ -37,8 +105,7 @@ function onLoad() {
     onConnected(connection, port.sender);
   });
   chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
-    connections[0].send(["getVariables"], sendResponse);
-    return true;
+    return onPopup(message.tabId, sendResponse);
   });
 }
 
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,20 +1,95 @@
+/**
+ * The data associated with displaying a single value.
+ */
+function DataEntry(valueElm) {
+  this.valueElm = valueElm;
+}
+
+DataEntry.prototype.update = function (value) {
+  this.valueElm.innerText = String(value);
+};
+
+/**
+ * The data for all entries.
+ */
+function DataDisplay() {
+  this.entries = {};
+}
+
+/**
+ * Returns the entry with the given name.
+ */
+DataDisplay.prototype.getEntry = function (name) {
+  return this.entries.hasOwnProperty(name) ? this.entries[name] : null;
+};
+
+/**
+ * Adds an entry with the given name.
+ */
+DataDisplay.prototype.addEntry = function (name, data) {
+  this.entries[name] = data;
+}
+
+DataDisplay.INSTANCE = new DataDisplay();
+
+/**
+ * Returns the singleton data display instance.
+ */
+DataDisplay.get = function () {
+  return this.INSTANCE;
+};
+
 function displayData(data) {
-  var root = document.body;
-  var ul = document.createElement("ul");
-  root.appendChild(ul);
+  var display = DataDisplay.get();
+  var builder = DomBuilder.attach(document.getElementById("container"));
   data.forEach(function (entry) {
-    var li = document.createElement("li");
-    ul.appendChild(li);
-    li.innerText = entry.name + ": " + entry.value;
-    li.title = entry.description;
+    var name = entry.name;
+    var dataEntry = display.getEntry(name);
+    if (!dataEntry) {
+      var valueElm;
+      builder
+        .begin("div")
+          .addClass("variable")
+          .begin("span")
+            .addClass("name")
+            .appendText(name)
+          .end()
+          .begin("div")
+            .addClass("value")
+            .delegate(function (_, elm) { valueElm = elm; })
+          .end()
+        .end();
+      dataEntry = new DataEntry(valueElm);
+      display.addEntry(name, dataEntry);
+    }
+    dataEntry.update(entry.value);
+  });
+}
+
+/**
+ * Returns the parameter with the given name or null.
+ */
+function getParameter(name) {
+  var params = window.location.search.split(/[?&]/);
+  var result = null;
+  params.forEach(function (param) {
+    var pair = param.split("=");
+    if (pair[0] == name)
+      result = pair[1];
+  });
+  return result;
+}
+
+function refreshDisplay(tabId) {
+  chrome.extension.sendMessage({tabId: tabId}, function (data) {
+    displayData(JSON.parse(data));
   });
 }
 
 function onLoad() {
-  chrome.extension.sendMessage({}, function (data) {
-    console.log(data);
-    displayData(JSON.parse(data));
-  });
+  var tabId = Number(getParameter("tabId"));
+  refreshDisplay(tabId);
+  window.setInterval(refreshDisplay.bind(null, tabId), 1000);
 }
 
 onLoad();
